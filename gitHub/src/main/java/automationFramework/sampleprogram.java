bc8b482a8483f316bb52171ff53a55bbc61904c7
Kark Raj <Kark Raj@192.168.13.1>
2022-06-28 21:24:47 +0530

    second

---
+++ gitHub/src/main/java/automationFramework/sampleprogram.java
@@ -0,0 +1,10 @@
+package automationFramework;
+
+public class sampleprogram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("today is sunday");
+	}
+
+}
