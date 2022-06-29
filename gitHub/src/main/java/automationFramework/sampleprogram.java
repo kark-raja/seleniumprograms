f5a2b8118cf2dcf9f83d425114a99cee656391f7
Kark Raj <Kark Raj@192.168.13.1>
2022-06-29 12:22:45 +0530

    thrid

--- gitHub/src/main/java/automationFramework/sampleprogram.java
+++ gitHub/src/main/java/automationFramework/sampleprogram.java
@@ -4,7 +4,7 @@ public class sampleprogram {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("today is sunday");
+System.out.println("sunday");
 	}
 
 }
