d80860219e98dc06f388856680029ff552a8255b
Kark Raj <Kark Raj@192.168.13.1>
2022-07-01 20:17:26 +0530

    last

--- gitHub/src/main/java/automationFramework/sampleprogram.java
+++ gitHub/src/main/java/automationFramework/sampleprogram.java
@@ -2,9 +2,9 @@ package automationFramework;
 
 public class sampleprogram {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-System.out.println("sunday");
-	}
+	
+	
+	
+	
 
 }
--- gitHub/src/test/resources/automationFramework/NewTest.java
+++ gitHub/src/test/resources/automationFramework/NewTest.java
@@ -1,9 +1,26 @@
 package automationFramework;
 
 import org.testng.annotations.Test;
+import org.openga.selenium.by;
 
 public class NewTest {
+	WebDriver driver;
   @Test
-  public void f() {
+  public void a_pageLoad() {
+	  System.setProperty("webdriver.chrome.driver","chromedriver");
+	  driver=new ChromeDriver();
+	  driver.get("http://www.saucedemo.com/");
+	  driver.manage().window().maximize();
+	  }
+  @Test
+  public void b_logIn() throws InterruptedException{
+	  driver.findElement(By.id("user-name")).sendKeys("standard-user");
+	  driver.findElement(By.name("password")).sendKeys("secret-sauce");
+	  driver.findElement(By.xpath("//input[@id_'login-button']")).click();
+	  Thread.sleep(5000);
   }
+  public void c_logOut() throws InterruptedException{
+	  driver.findElement(By.id("react-burger-menu-btn")).click();
+	  Thread.sleep(5000);
+	  driver.findElement(By.id("logout_sidebar_link")).click();
 }
